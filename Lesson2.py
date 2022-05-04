0f6aed47e8340f432de34e48807ea67d22a145af
Korper78 <korper78@gmail.com>
2022-05-04 15:42:50 +0300

    Oh my God!!!!

--- Lesson2.py
+++ Lesson2.py
@@ -1,4 +1,14 @@
-print('WTF')
-print('>:((((')
-print('f*kc((((')
-print('New branch')
+# Добавляем
+InString = input('А введи-ка целое число: ')
+Digits = len(InString)
+#print(Digits)
+if not InString.isdigit():
+    OutString = 'Ты меня за дурака держишь? >:((('
+else:
+    if Digits % 10 == 1 and Digits != 11:
+        OutString = 'В этом числе '+str(Digits)+' цифра'
+    elif Digits % 10 in [2, 3, 4] and Digits not in [12, 13, 14]:
+        OutString = 'В этом числе ' + str(Digits) + ' цифры'
+    else:
+        OutString = 'В этом числе ' + str(Digits) + ' цифр'
+print(OutString)
