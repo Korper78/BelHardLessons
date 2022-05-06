2253b4961a39f9af784af0116d10b072fd41aa5b
Korper78 <korper78@gmail.com>
2022-05-06 12:37:19 +0300

    Coooool!

--- Lesson2.py
+++ Lesson2.py
@@ -6,9 +6,9 @@ if not InString.isdigit():
     OutString = 'Ты меня за дурака держишь? >:((('
 else:
     if Digits % 10 == 1 and Digits != 11:
-        OutString = 'В этом числе '+str(Digits)+' цифра'
+        OutString = 'В этом числе {Digits} цифра'
     elif Digits % 10 in [2, 3, 4] and Digits not in [12, 13, 14]:
-        OutString = 'В этом числе ' + str(Digits) + ' цифры'
+        OutString = f'В этом числе {Digits} цифры'
     else:
-        OutString = 'В этом числе ' + str(Digits) + ' цифр'
+        OutString = f'В этом числе {Digits} цифр'
 print(OutString)
