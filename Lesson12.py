ded5ebfd50fdf8e6521bb4639d49e0c85040537c
Korper78 <korper78@gmail.com>
2022-06-06 15:46:23 +0300

    Lesson 12 Homework. Ya sdielal'!)))) В базу currency.db записывают: Lesson11.py - в таблицу currency курсы USD, EUR, RUB на текущую дату, взятые из API сайта Нацбанка РБ Lesson12.py - заполняет таблицы Category и Product, и печатает запросы из них

--- Lesson12.py
+++ Lesson12.py
@@ -2,17 +2,15 @@ import sqlite3
 from collections import namedtuple
 
 
-Category = namedtuple('Category', ['ID', 'Name', 'Parent', 'Descr'])
+Category = namedtuple('Category', ['Name', 'Parent', 'Descr'])
 categories = Category(
-    [1, 2],
     ['Cat1', 'Cat2'],
     [1, 2],
     ['Category one', 'Category two']
 )
 
-Product = namedtuple('Product', ['ID', 'Name', 'Descr', 'Category'])
+Product = namedtuple('Product', ['Name', 'Descr', 'Category'])
 products = Product(
-    [1, 2, 3, 4],
     ['Prod1', 'Prod2', 'Prod3', 'Prod4'],
     ['Product one', 'Product two', 'Product three', 'Product four'],
     [1, 2, 1, 2]
@@ -22,20 +20,23 @@ products = Product(
 # print(*products)
 
 sql_db = sqlite3.connect('currency.db')
-sql_db.execute("create table if not exists category (id int primary key, name text unique, parent int, descr text)")
-sql_db.execute("create table if not exists product (id int primary key, name text, descr text, category int not null)")
+sql_db.execute("create table if not exists category (id integer primary key autoincrement, name text unique, parent int, descr text);")
+sql_db.execute("create table if not exists product (id integer primary key autoincrement, name text, descr text, category int not null);")
+sql_db.commit()
 db_cur = sql_db.cursor()
 for indx in range(2):
-    db_cur.execute("replace into category (id, name, parent, descr) values(?,?,?,?)", (categories.ID[indx], categories.Name[indx], categories.Parent[indx], categories.Descr[indx]))
-db_cur.execute("select * from category where id=?", (1,))
+    db_cur.execute("insert into category (name, parent, descr) values(?,?,?);", (categories.Name[indx], categories.Parent[indx], categories.Descr[indx]))
+    sql_db.commit()
+db_cur.execute("select * from category where id=?;", (1,))
 print(db_cur.fetchall())
-db_cur.execute("select * from category where name=?", ('Cat2',))
+db_cur.execute("select * from category where name=?;", ('Cat2',))
 print(db_cur.fetchall())
 for indx in range(4):
-    db_cur.execute("replace into product (id, name, descr, category) values(?,?,?,?)", (products.ID[indx], products.Name[indx], products.Descr[indx], products.Category[indx]))
-db_cur.execute("select * from product where id=?", (3,))
+    db_cur.execute("insert into product (name, descr, category) values(?,?,?);", (products.Name[indx], products.Descr[indx], products.Category[indx]))
+    sql_db.commit()
+db_cur.execute("select * from product where id=?;", (3,))
 print(db_cur.fetchall())
-db_cur.execute("select * from product where category=?", (1,))
+db_cur.execute("select * from product where category=?;", (1,))
 print(db_cur.fetchall())
 
 sql_db.commit()
